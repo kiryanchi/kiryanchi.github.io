8a39456b1f6ec68306abbaede0113422eb4183b2
kiryanchi <kiryanchi@naver.com>
2023-09-11 15:52:15 +0900

    profile img

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230911.141811';
+const cacheName = 'chirpy-20230911.154916';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
